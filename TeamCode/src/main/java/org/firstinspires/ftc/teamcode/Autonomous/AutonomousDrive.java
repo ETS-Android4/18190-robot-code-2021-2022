2edbacb3a9a4a393982ccd12cd3aa839abf8f77f
matursam <mtursam@gmail.com>
2021-12-06 20:38:42 -0500

    Added gyro turning code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutonomousDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutonomousDrive.java
@@ -1,17 +1,31 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.firstinspires.ftc.teamcode.Hardware.HWDriveTrain;
+
+import java.util.Locale;
 
 
 @Autonomous(name = "autonomousDrive")
 public class AutonomousDrive extends LinearOpMode {
     HWDriveTrain hwDriveTrain;
+
+    BNO055IMU imu;
+
+    Orientation angles;
+
     public ElapsedTime runtime = new ElapsedTime();
 
     static final double COUNTS_PER_MOTOR_REV = 1440;    // eg: TETRIX Motor Encoder
@@ -23,9 +37,14 @@ public class AutonomousDrive extends LinearOpMode {
     static final double TURN_SPEED = 0.5;
 
 
+    static final double TURNING_P = 0.015;
+
     @Override
     public void runOpMode() {
-        hwDriveTrain.init(hardwareMap);
+
+        hwDriveTrain = new HWDriveTrain();
+
+        hwDriveTrain.init(this.hardwareMap);
 
         telemetry.addData("Status", "Resetting Encoders");    //
         telemetry.update();
@@ -41,6 +60,17 @@ public class AutonomousDrive extends LinearOpMode {
         hwDriveTrain.leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         hwDriveTrain.rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled      = true;
+        parameters.loggingTag          = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+
         telemetry.addData("Path0", "Starting at %7d :%7d",
                 hwDriveTrain.leftFront.getCurrentPosition(),
                 hwDriveTrain.rightFront.getCurrentPosition(),
@@ -51,10 +81,58 @@ public class AutonomousDrive extends LinearOpMode {
 
         waitForStart();
 
+        turnToPosition(90);
+
 
 
 
     }
+
+    public void turnToPosition(double target) {
+
+        double current_angle = getHeading();
+
+        target = current_angle + target;
+
+        double error = target - current_angle;
+
+        int valid_cycles = 0;
+
+        while( error > 1 && valid_cycles > 10) {
+            current_angle = getHeading();
+            error = target - current_angle;
+            double power = error * TURNING_P;
+
+            hwDriveTrain.leftBack.setPower(power);
+            hwDriveTrain.leftFront.setPower(power);
+            hwDriveTrain.rightBack.setPower(-power);
+            hwDriveTrain.rightFront.setPower(-power);
+
+            telemetry.addData("Heading: ", current_angle);
+            telemetry.addData("Error: ", error);
+            telemetry.addData("Power: ", power);
+            telemetry.update();
+
+            if (error < 1) {
+                valid_cycles++;
+            }
+            else {
+                valid_cycles = 0;
+            }
+        }
+
+        hwDriveTrain.leftBack.setPower(0);
+        hwDriveTrain.leftFront.setPower(0);
+        hwDriveTrain.rightBack.setPower(0);
+        hwDriveTrain.rightFront.setPower(0);
+
+        telemetry.addData("Finished Turning", "Printing final values");
+        telemetry.addData("Heading: ", current_angle);
+        telemetry.addData("Error: ", error);
+        telemetry.update();
+    }
+
+
     public void encoderDrive(double speed,
                              double leftInches, double rightInches,
                              double timeoutS) {
@@ -115,4 +193,11 @@ public class AutonomousDrive extends LinearOpMode {
             //  sleep(250);   // optional pause after each move
         }
     }
+
+  public double getHeading() {
+      angles   = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+      return AngleUnit.DEGREES.fromUnit(angles.angleUnit, angles.firstAngle);
+  }
+
+
 }
