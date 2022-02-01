f24cfbba5b538f1b18cbea9fe974d788f112fe19
matursam <mtursam@gmail.com>
2022-01-31 21:11:35 -0500

    Most recent code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/HWDriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/HWDriveTrain.java
@@ -1,21 +1,18 @@
 package org.firstinspires.ftc.teamcode.Hardware;
 
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.CRServoImplEx;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.VoltageSensor;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
-import java.util.ArrayList;
 
 public class HWDriveTrain {
     public DcMotorEx leftFront;
@@ -24,6 +21,10 @@ public class HWDriveTrain {
     public DcMotorEx rightBack;
 
     public DcMotorEx duckMotor;
+
+    public DcMotorEx armMotor;
+
+    public CRServo collector;
 
     public  double slowDown = 1;
 
@@ -46,39 +47,47 @@ public class HWDriveTrain {
 
         duckMotor = (DcMotorEx) hwMap.get(DcMotor.class, "duck");
 
+        armMotor = (DcMotorEx) hwMap.get(DcMotor.class, "arm");
+
+        //collector = hwMap.crservo.get("collector");
+        collector = hwMap.crservo.get("collector");
+
         leftFront.setDirection(DcMotorSimple.Direction.FORWARD);
         rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
         leftBack.setDirection(DcMotorSimple.Direction.FORWARD);
         rightBack.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        //armMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
         leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         duckMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         duckMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-        leftFront.setPower(0);
-        rightFront.setPower(0);
-        leftBack.setPower(0);
-        rightBack.setPower(0);
-        duckMotor.setPower(0);
-    }
-
-    public void teleOpMove(double y1, double x1, double x2, boolean isSlowdown) {
-        double headingPower = 0;
-        double headingAngle = 0;
-        double turn = x2 * 0.5;
 
         leftFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rightFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         leftBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rightBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
+        leftFront.setPower(0);
+        rightFront.setPower(0);
+        leftBack.setPower(0);
+        rightBack.setPower(0);
+        duckMotor.setPower(0);
+
+        //collector.setPower(0);
+    }
+
+    public void teleOpMove(double y1, double x1, double x2, boolean isSlowdown) {
+        double turn = x2 * 0.5;
+
         //Convert cartesian coordinates to polar coordinates
-        headingPower = Math.hypot(x1, -y1);
-        headingAngle = Math.atan2(x1, -y1)  + Math.PI/4;
+        double headingPower = Math.hypot(x1, -y1);
+        double headingAngle = Math.atan2(x1, -y1)  + Math.PI/4;
 
         slowDown = isSlowdown ? 0.5 : 1;
 
@@ -87,38 +96,12 @@ public class HWDriveTrain {
         double leftBackPower = (headingPower * Math.sin(headingAngle) - turn);
         double rightBackPower = (headingPower * Math.cos(headingAngle) + turn);
 
-        //double maxPowerRaw = Math.max(Math.abs(leftFrontPowerRaw), Math.max(Math.abs(rightFrontPowerRaw), Math.max(Math.abs(leftBackPowerRaw), Math.abs(rightBackPowerRaw))));
-
-        /*
-
-        if (maxPowerRaw > 1) {
-            leftFrontPowerScaled = leftFrontPowerRaw / maxPowerRaw;
-            rightFrontPowerScaled = rightFrontPowerRaw / maxPowerRaw;
-            leftBackPowerScaled = leftBackPowerRaw / maxPowerRaw;
-            rightBackPowerScaled = rightBackPowerRaw / maxPowerRaw;
-        } else {
-            leftFrontPowerScaled = leftFrontPowerRaw;
-            rightFrontPowerScaled = rightFrontPowerRaw;
-            leftBackPowerScaled = leftBackPowerRaw;
-            rightBackPowerScaled = rightBackPowerRaw;
-        }
-
-         */
 
 
         leftFront.setPower(leftFrontPower * slowDown);
         rightFront.setPower(rightFrontPower * slowDown);
         leftBack.setPower(leftBackPower * slowDown);
         rightBack.setPower(rightBackPower * slowDown);
-
-        ArrayList<Double> velocities = new ArrayList<>();
-        velocities.add(((DcMotorEx) leftFront).getVelocity());
-        velocities.add(((DcMotorEx) leftBack).getVelocity());
-        velocities.add(((DcMotorEx) rightFront).getVelocity());
-        velocities.add(((DcMotorEx) rightBack).getVelocity());
-
-        telemetry.addData("Wheel Velocities: ", velocities.toString());
-        telemetry.update();
 
     }
 
@@ -140,17 +123,6 @@ public class HWDriveTrain {
         rightFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rightBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-    }
-
-    public double getBatteryVoltage() {
-        double result = Double.POSITIVE_INFINITY;
-        for (VoltageSensor sensor : hardwareMap.voltageSensor) {
-            double voltage = sensor.getVoltage();
-            if (voltage > 0) {
-                result = Math.min(result, voltage);
-            }
-        }
-        return result;
     }
 }
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimpleDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimpleDrive.java
@@ -17,6 +17,37 @@ public class SimpleDrive extends OpMode {
 
     @Override
     public void loop() {
-       hwDriveTrain.teleOpMove(-gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x, gamepad1.left_bumper);
+        hwDriveTrain.teleOpMove(-gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x, gamepad1.left_bumper);
+
+        if (gamepad1.left_trigger > 0) {
+            hwDriveTrain.duckMotor.setPower(-gamepad1.left_trigger);
+        }
+        else {
+            hwDriveTrain.duckMotor.setPower(gamepad1.right_trigger);
+        }
+
+        // Arm Control
+        double armPower = gamepad2.left_stick_y * 0.5;
+        if (Math.abs(armPower) < 0.1) {
+            armPower = 0;
+        }
+        hwDriveTrain.armMotor.setPower(armPower);
+
+        double collectorPower;
+        // Collection
+        if (gamepad2.right_stick_y > 0) {
+            collectorPower = Math.log(gamepad2.right_stick_y + 1) * 3.4;
+        }
+        else if (gamepad2.right_stick_y < 0) {
+            collectorPower = -Math.log(Math.abs(gamepad2.right_stick_y) + 1) * 3.4;
+        }
+        else{
+            collectorPower = 0;
+        }
+        hwDriveTrain.collector.setPower(collectorPower);
+
+        telemetry.addData("collector", collectorPower);
+        telemetry.update();
+
     }
 }
